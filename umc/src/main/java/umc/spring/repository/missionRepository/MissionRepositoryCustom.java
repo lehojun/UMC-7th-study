37d1a30cde12f16bb3be4080d80190e3cb1e2333
MeongW <easymiracle@skuniv.ac.kr>
2024-11-14 17:42:21 +0900

    week6

---
+++ umc/src/main/java/umc/spring/repository/missionRepository/MissionRepositoryCustom.java
@@ -0,0 +1,2 @@
+package umc.spring.repository.missionRepository;public class MissionRepositoryCustom {
+}
---
+++ umc/src/main/java/umc/spring/repository/missionRepository/MissionRepositoryImpl.java
@@ -0,0 +1,4 @@
+package umc.spring.repository.missionRepository;
+
+public class MissionRepositoryImpl {
+}
