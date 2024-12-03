b81b7b8a76006530d7f61fd6ed39048c47c8f79a
lehojun <hojunchris@naver.com>
2024-12-03 10:30:56 +0900

    [FEAT]week9 실습

--- src/main/java/umc/spring/converter/StoreConverter.java
+++ src/main/java/umc/spring/converter/StoreConverter.java
@@ -1,14 +1,22 @@
 package umc.spring.converter;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import umc.spring.domain.Member;
 import umc.spring.domain.Region;
+import umc.spring.domain.Review;
 import umc.spring.domain.Store;
 import umc.spring.web.dto.MemberDTO.MemberResponseDTO;
+import umc.spring.web.dto.ReviewDTO.ReviewResponseDTO;
 import umc.spring.web.dto.StoreDTO.StoreRequestDTO;
 import umc.spring.web.dto.StoreDTO.StoreResponseDTO;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import static org.aspectj.bridge.MessageUtil.print;
+@Slf4j
 public class StoreConverter {
 
     public static Store toStore(StoreRequestDTO.AddDto request) {
@@ -24,6 +32,30 @@ public class StoreConverter {
                 .storeId(store.getId())
                 .createdAt(store.getCreatedAt())
                 .regionId(store.getRegion().getId())
+                .build();
+    }
+
+    public static ReviewResponseDTO.ReviewPreViewDTO reviewPreViewDTO(Review review) {
+        log.info("reviewPreViewDTO 호출: {}", review);
+        return   ReviewResponseDTO.ReviewPreViewDTO.builder()
+                .ownerNickname(review.getMember().getName())
+                .score(review.getScore())
+                .createdAt(review.getCreatedAt().toLocalDate())
+                .body(review.getTitle())
+                .build();
+    }
+    public static ReviewResponseDTO.ReviewPreViewListDTO reviewPreViewListDTO(Page<Review> reviewList){
+        log.info("reviewList 호출: {}",reviewList);
+        List<ReviewResponseDTO.ReviewPreViewDTO> reviewPreViewDTOList = reviewList.stream()
+                .map(StoreConverter::reviewPreViewDTO).collect(Collectors.toList());
+
+        return ReviewResponseDTO.ReviewPreViewListDTO.builder()
+                .isLast(reviewList.isLast())
+                .isFirst(reviewList.isFirst())
+                .totalPage(reviewList.getTotalPages())
+                .totalElements(reviewList.getTotalElements())
+                .listSize(reviewPreViewDTOList.size())
+                .reviewList(reviewPreViewDTOList)
                 .build();
     }
 }
--- src/main/java/umc/spring/service/StoreService/StoreQueryServiceImpl.java
+++ src/main/java/umc/spring/service/StoreService/StoreQueryServiceImpl.java
@@ -1,9 +1,16 @@
 package umc.spring.service.StoreService;
 
 import lombok.RequiredArgsConstructor;
+import org.hibernate.dialect.HANACloudColumnStoreDialect;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import umc.spring.apiPayload.code.status.ErrorStatus;
+import umc.spring.apiPayload.exception.handler.StoreHandler;
+import umc.spring.domain.Review;
 import umc.spring.domain.Store;
+import umc.spring.repository.ReviewRepository.ReviewRepository;
 import umc.spring.repository.StoreRepository.StoreRepository;
 
 import java.util.List;
@@ -14,6 +21,7 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public class StoreQueryServiceImpl implements StoreQueryService{
     private final StoreRepository storeRepository;
+    private final ReviewRepository reviewRepository;
 
 
     @Override
@@ -28,5 +36,13 @@ public class StoreQueryServiceImpl implements StoreQueryService{
         filteredStores.forEach(store -> System.out.println("Store: " + store));
 
         return filteredStores;
+    }
+
+    @Override
+    public Page<Review> getReviewList(Long storeId, Integer page) {
+        Store store = storeRepository.findById(storeId)
+                .orElseThrow(() -> new StoreHandler(ErrorStatus.STORE_NOT_FOUND));
+
+        return reviewRepository.findAllByStore(store, PageRequest.of(page, 10));
     }
 }
